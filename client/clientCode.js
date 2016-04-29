25974ba8f9bb04682a76844799c46fbf729fd748
DarkAce65 <tvdood99@gmail.com>
2016-04-28 21:54:13 -0400

    Reorder buttons

--- client/clientCode.js
+++ client/clientCode.js
@@ -1,13 +1,13 @@
 Template.login.events({
-	"click #googleLogin": function(e) {
-		Meteor.loginWithGoogle(function(error) {
+	"click #facebookLogin": function(e) {
+		Meteor.loginWithFacebook(function(error) {
 			if(error) {
 				alert(error);
 			}
 		});
 	},
-	"click #facebookLogin": function(e) {
-		Meteor.loginWithFacebook(function(error) {
+	"click #googleLogin": function(e) {
+		Meteor.loginWithGoogle(function(error) {
 			if(error) {
 				alert(error);
 			}

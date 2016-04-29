fd97f1b4dd81790edd04749a3b51df748901f40b
DarkAce65 <tvdood99@gmail.com>
2016-04-28 20:24:52 -0400

    Create assignInitialTargets method

--- server/methods.js
+++ server/methods.js
@@ -1,3 +1,28 @@
-Meteor.methods({
+function shuffle(array) { // Fisher–Yates Shuffle
+	var m = array.length, t, i;
+	while(m) {
+		i = Math.floor(Math.random() * m--);
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+	return array;
+}
 
+Meteor.methods({
+	"assignInitialTargets": function() {
+		var userIdList = Meteor.users.find({}, {fields: {"_id": 1}}).fetch().map(function(value) {
+			return value._id;
+		});
+		shuffle(userIdList);
+
+		for(var i = 0; i < userIdList.length; i++) {
+			var shift = (i + 1) % userIdList.length;
+			Meteor.users.update(userIdList[i], {
+				$set: {
+					"target": userIdList[shift]
+				}
+			});
+		}
+	}
 });

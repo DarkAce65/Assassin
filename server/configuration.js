dd5e77f8d83b009083989996ee2ad8fe9e331224
DarkAce65 <tvdood99@gmail.com>
2016-04-30 15:33:11 -0400

    Configure user object on creation

--- server/configuration.js
+++ server/configuration.js
@@ -1,7 +1,19 @@
+Accounts.onCreateUser(function(options, user) {
+	user.admin = false;
+	user.inGame = false;
+
+	if(options.profile) {
+		user.profile = options.profile;
+	}
+
+	return user;
+});
+
 Meteor.publish("userInfo", function() {
 	return Meteor.users.find(this.userId, {
 		fields: {
 			"admin": 1,
+			"inGame": 1,
 			"alive": 1,
 			"target": 1,
 			"assassinations": 1

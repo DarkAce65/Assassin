653a6005d68d75a9974ec5d0268606b554ddf828
DarkAce65 <tvdood99@gmail.com>
2016-04-30 23:22:51 -0400

    Add adminPanel and admin subscription

--- server/configuration.js
+++ server/configuration.js
@@ -8,6 +8,26 @@ Accounts.onCreateUser(function(options, user) {
 	return user;
 });
 
+Meteor.publish("adminInfo", function() {
+	if(!Roles.userIsInRole(this.userId, "admin")) {
+		this.ready();
+	}
+
+	return [
+		Meteor.users.find({"inGame": true}, {
+			fields: {
+				"alive": 1,
+				"inGame": 1,
+				"kills": 1,
+				"assassin": 1,
+				"target": 1,
+				"profile.name": 1
+			}
+		}),
+		Actions.find()
+	];
+});
+
 Meteor.publish("target", function() {
 	return Meteor.users.find(this.userId, {
 		fields: {

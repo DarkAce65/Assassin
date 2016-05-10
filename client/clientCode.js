89107bf1634038a91d31196a69124936adfc7bcb
DarkAce65 <tvdood99@gmail.com>
2016-05-09 21:53:22 -0400

    Remove Facebook login event

--- client/clientCode.js
+++ client/clientCode.js
@@ -28,16 +28,6 @@ Template.navigation.events({
 });
 
 Template.login.events({
-	"click #facebookLogin": function(e) {
-		Meteor.loginWithFacebook(function(error) {
-			if(error) {
-				alert(error);
-			}
-			else {
-				Router.go("target");
-			}
-		});
-	},
 	"click #googleLogin": function(e) {
 		Meteor.loginWithGoogle(function(error) {
 			if(error) {

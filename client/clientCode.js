9b40e83720acd7861de3c28a7641ea40abdf654b
DarkAce65 <tvdood99@gmail.com>
2016-05-18 15:20:13 -0400

    Edit leaderboard sort order

--- client/clientCode.js
+++ client/clientCode.js
@@ -299,6 +299,6 @@ Template.target.events({
 
 Template.leaderboard.helpers({
 	"userList": function() {
-		return Meteor.users.find({"inGame": true}, {sort: {"kills": -1}}).fetch();
+		return Meteor.users.find({"inGame": true}, {sort: {"kills": -1, "alive": -1, "profile.name": 1}}).fetch();
 	}
 });
